61fffbd83c2cd6c655ac923ae799cf3abe31b5c8
Esraa Abdelnaby <18484007+Esraa772@users.noreply.github.com>
2019-01-29 23:18:08 +0200

    Add files via upload

---
+++ micro.h
@@ -0,0 +1,88 @@
+#ifndef _M_H
+#define _M_H
+//Define port A Directions  & Values (tris and port)
+#define dira_0 TrisA.b0
+#define dira_1 TrisA.b1
+#define dira_2 TrisA.b2
+#define dira_3 TrisA.b3
+#define dira_4 TrisA.b4
+#define dira_5 TrisA.b5
+
+#define vala_0 portA.b0
+#define vala_1 portA.b1
+#define vala_2 portA.b2
+#define vala_3 portA.b3
+#define vala_4 portA.b4
+#define vala_5 portA.b5
+//Define Port B Directions & values
+#define dirb_0 TRISB.B0
+#define dirb_1 TRISB.B1
+#define dirb_2 TRISB.B2
+#define dirb_3 TRISB.B3
+#define dirb_4 TRISB.B4
+#define dirb_5 TRISB.B5
+#define dirb_6 TRISB.B6
+#define dirb_7 TRISB.B7
+
+#define valb_0 PORTB.B0
+#define valb_1 PORTB.B1
+#define valb_2 PORTB.B2
+#define valb_3 PORTB.B3
+#define valb_4 PORTB.B4
+#define valb_5 PORTB.B5
+#define valb_6 PORTB.B6
+#define valb_7 PORTB.B7
+
+//Define Port C Directions & values
+#define dirc_0  TrisC.b0
+#define dirc_1  TrisC.b1
+#define dirc_2  TrisC.b2
+#define dirc_3  TrisC.b3
+#define dirc_4  TrisC.b4
+#define dirc_5  TrisC.b5
+#define dirc_6  TrisC.b6
+#define dirc_7  TrisC.b7
+
+#define valc_0  portC.b0
+#define valc_1  portC.b1
+#define valc_2  portC.b2
+#define valc_3  portC.b3
+#define valc_4  portC.b4
+#define valc_5  portC.b5
+#define valc_6  portC.b6
+#define valc_7  portC.b7
+
+//Define Port D Directions & values
+#define dird_0  TrisD.b0
+#define dird_1  TrisD.b1
+#define dird_2  TrisD.b2
+#define dird_3  TrisD.b3
+#define dird_4  TrisD.b4
+#define dird_5  TrisD.b5
+#define dird_6  TrisD.b6
+#define dird_7  TrisD.b7
+
+#define vald_0  PortD.b0
+#define vald_1  PortD.b1
+#define vald_2  PortD.b2
+#define vald_3  PortD.b3
+#define vald_4  PortD.b4
+#define vald_5  PortD.b5
+#define vald_6  PortD.b6
+#define vald_7  PortD.b7
+
+//Define port E
+#define dire_0  TrisE.b0
+#define dire_1  TrisE.b1
+#define dire_2  TrisE.b2
+
+#define vale_0  PortE.b0
+#define vale_1  PortE.b1
+#define vale_2  PortE.b2
+
+//---------------------------------------------------------
+#define input 1
+#define output 0
+#define low 0
+#define high 1
+#endif
---
+++ port.h
@@ -0,0 +1,12 @@
+#ifndef _P_H
+#define _P_H
+#include "micro.h"
+#define ledA_dir dirb_1
+#define ledA_val valb_1
+#define ledB_dir dirb_2
+#define ledB_val valb_2
+#define ledC_dir dirb_4
+#define ledC_val valb_4
+#define ledD_dir dirb_5
+#define ledD_val valb_5
+#endif
